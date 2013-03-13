f041d096177bbc93436fb21f5a4736fbf37477b0
Zack Maril <zack@zacharymaril.com>
2013-03-13 13:27:59 +0400

    range and retain

---
+++ test/ogre/filter/retain_test.clj
@@ -0,0 +1,22 @@
+(ns ogre.filter.retain-test
+  (:use [clojure.test])
+  (:require [ogre.core :as q]
+            [ogre.test-util :as g]))
+
+(deftest test-range-step
+  (g/use-new-tinker-graph!)
+  (testing "test_g_v1_out_retainXg_v2X"
+    (let [vs (q/query (g/find-by-id 1)
+                      (q/-->)
+                      (q/retain [(g/find-by-id 2)])
+                      (q/into-vec))]
+      (is (= 1 (count vs)))
+      (is (= "vadas" (g/get-property :name (first vs))))))
+
+  (testing "test_g_v1_out_aggregateXxX_out_retainXxX"
+    (let [vs (q/query (g/find-by-id 1)
+                      (q/-->)
+                      (q/retain [(g/find-by-id 2)])
+                      (q/into-vec))]
+      ;;TODO finish this once aggregrate is finished
+)))                        
