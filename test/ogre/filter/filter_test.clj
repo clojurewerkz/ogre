0d47bc12ede6bb8be2639815674ad0caaa9502a8
Zack Maril <zack@zacharymaril.com>
2013-03-13 12:46:02 +0400

    Filter test

---
+++ test/ogre/filter/filter_test.clj
@@ -0,0 +1,49 @@
+(ns ogre.filter.filter-test
+  (:use [clojure.test])
+  (:require [ogre.core :as q]
+            [ogre.test-util :as g]))
+
+(defn prop-pred [key pred value v]
+  (pred value (g/get-property key v)))
+
+(deftest test-filter-step
+  (g/use-new-tinker-graph!)
+  (testing "test_g_V_filterXfalseX"
+    (let [vs (q/query (g/get-vertices)
+                      (q/filter (constantly false))                    
+                      (q/into-vec))]
+      (is (= 0 (count vs)))))
+
+  (testing "test_g_V_filterXtrueX"
+    (let [vs (q/query (g/get-vertices)
+                      (q/filter (constantly true))                    
+                      (q/into-vec))]
+      (is (= 6 (count vs)))))
+  
+  (testing "test_g_V_filterXlang_eq_javaX()"
+    (let [vs (q/query (g/get-vertices)
+                      (q/filter (partial prop-pred :lang = "java"))                    
+                      (q/into-vec))]
+      (is (= 2 (count vs)))
+      (is (= #{"lop" "ripple"} (set (map (partial g/get-property :name) vs))))))
+
+  (testing "test_g_v1_out_filterXage_gt_30X"
+    (let [vs (q/query (g/find-by-id 1)
+                      (q/-->)
+                      (q/filter (fn [v]
+                                  (let [age (g/get-property :age v)]
+                                    (and (not (nil? age)) (> age 30)))))                    
+                      (q/into-vec))]
+      (is (= 1 (count vs)))
+      (is (= #{32} (set (map (partial g/get-property :age) vs))))))
+
+  (testing "test_g_V_filterXname_startsWith_m_OR_name_startsWith_pX()"
+    (let [vs (q/query (g/get-vertices)
+                      (q/filter #(->> %
+                                    (g/get-property :name)
+                                    first
+                                    #{\m \p}
+                                    boolean))                    
+                      (q/into-vec))]
+      (is (= 2 (count vs)))
+      (is (= #{"marko" "peter"} (set (map (partial g/get-property :name) vs)))))))
