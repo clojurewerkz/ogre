415007ca53d8d3a81a2d6508148e7fcf71fd3df9
Zack Maril <zack@zacharymaril.com>
2013-04-02 02:35:39 +0400

    Problems using snapshots for the moment.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/ogre "2.3.0.1-SNAPSHOT"
+(defproject clojurewerkz/ogre "2.3.0.1"
   :description "Clojure wrapper around gremlin"
   :url "https://github.com/clojurewerkz/ogre"
   :license {:name "Eclipse Public License"
