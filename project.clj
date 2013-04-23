767e670aac7f2f0b3cb94ad7f20075e25fa414b0
Zack Maril <zack@zacharymaril.com>
2013-04-23 14:23:43 +0400

    Pegging 2.3.0.1 and moving onto the next version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/ogre "2.3.0.1"
+(defproject clojurewerkz/ogre "2.3.0.2"
   :description "Clojure wrapper around gremlin"
   :url "https://github.com/clojurewerkz/ogre"
   :license {:name "Eclipse Public License"
