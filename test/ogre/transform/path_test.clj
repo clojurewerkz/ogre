8a8d51b6790dd75297708e61420a4da6c211ad90
Zack Maril <zack@zacharymaril.com>
2013-05-02 01:16:51 +0400

    Uncommented the path loop test and made it run.

--- test/ogre/transform/path_test.clj
+++ test/ogre/transform/path_test.clj
@@ -23,25 +23,27 @@
           names  (map second path)]
       (is (= [29 29 29] age))
       (is (= ["vadas" "josh" "lop"] names))))
+  (testing "g.getVertex(1).out.path{it.age}{it.name}"
+    (let [path (q/query (g/find-by-id 1)
+                        q/-->
+                        (q/path
+                         (partial g/get-property :age)
+                         (partial g/get-property :name))
+                        q/all-into-vecs!)
+          age  (map first path)
+          names  (map second path)]
+      (is (= [29 29 29] age))
+      (is (= ["vadas" "josh" "lop"] names))))
 
-  ;;TODO write this test once loop has been finished
-  ;; GremlinPipeline(g.getVertices()).out().loop(1, new PipeFunction<LoopPipe.LoopBundle, Boolean>() {
-;;             public Boolean compute(LoopPipe.LoopBundle bundle) {
-;;                 return bundle.getLoops() < 3;
-;;             }
-;;         }).path(new PipeFunction<Vertex, Vertex>() {
-;;                     public Vertex compute(Vertex vertex) {
-;;                         return vertex;
-;;                     }
-;;                 }, new PipeFunction<Vertex, String>() {
-;;                     public String compute(Vertex vertex) {
-;;                         return (String) vertex.getProperty("name");
-;;                     }
-;;                 }, new PipeFunction<Vertex, String>() {
-;;                     public String compute(Vertex vertex) {
-;;                         return (String) vertex.getProperty("lang");
-;;                     }
-;;                 }
-;;         ));
-
-  )
+  (testing "g.V.out.loop(1,loops_lt_3)X_path{it.name,it.lang}"
+    (let [path (q/query (g/get-vertices)
+                        q/-->
+                        (q/loop 1 (fn [i o p] (< i 3)))
+                        (q/path identity
+                                (partial g/get-property :name)
+                                (partial g/get-property :lang))
+                        q/all-into-vecs!)
+          age  (map first path)
+          names  (map second path)]
+      (is (= 3 (count (first path))))
+      (is (= (rest (first path)) '("josh" "java"))))))
