ebe55245910a3c7cab09a1fab5d1d598585c6c13
Ray Miller <ray@1729.org.uk>
2014-09-27 15:32:52 +0100

    Support arity 2 has and has-not filters.

--- src/clojure/clojurewerkz/ogre/filter.clj
+++ src/clojure/clojurewerkz/ogre/filter.clj
@@ -5,26 +5,32 @@
   (:require [clojurewerkz.ogre.util :refer (convert-symbol-to-compare f-to-pipef)]))
 
 (defn dedup
-  ([p]   
+  ([p]
      (conj p #(.dedup ^GremlinPipeline %)))
-  ([p f] 
+  ([p f]
      (conj p #(.dedup ^GremlinPipeline % (f-to-pipef f)))))
 
 (defmacro has
+  ([p k]
+     `(conj ~p (fn [parg#] (.has parg# ~(name k)))))
   ([p k v]
      `(conj ~p (fn [parg#] (.has parg# ~(name k) ~v))))
   ([p k c v]
-     `(conj ~p (fn [parg#] (.has parg# ~(name k) 
+     `(conj ~p (fn [parg#] (.has parg# ~(name k)
                                  (convert-symbol-to-compare '~c)
                                  ~v)))))
 
+;; Note: as of Blueprints 2.5.0, Gremlin does not support a comparotor
+;; for the .hasNot filter.
 (defmacro has-not
-  [p k v] 
-  `(conj ~p (fn [parg#] (.hasNot parg# ~(name k) ~v))))
+  ([p k]
+     `(conj ~p (fn [parg#] (.hasNot parg# ~(name k)))))
+  ([p k v]
+     `(conj ~p (fn [parg#] (.hasNot parg# ~(name k) ~v)))))
 
-(defn interval 
+(defn interval
   [p key start end]
-  (conj p #(.interval ^GremlinPipeline % 
-                      ^String (name key) 
-                      ^Float  (float start) 
+  (conj p #(.interval ^GremlinPipeline %
+                      ^String (name key)
+                      ^Float  (float start)
                       ^Float  (float end))))
