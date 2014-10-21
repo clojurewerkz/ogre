195ebf7eadeb68144d97fa8d9ca6900860240917
Stephen Mallette <spmva@genoprime.com>
2014-10-21 15:47:20 -0400

    Migrated all query_test tests to traversal_test.
    There no longer is a direct notion of vertex query in tp3.  it's all just a traversal.

--- test/clojurewerkz/ogre/map/traversal_test.clj
+++ test/clojurewerkz/ogre/map/traversal_test.clj
@@ -120,3 +120,119 @@
                       q/-->
                       q/into-vec!)]
       (is (= 0 (count vs))))))
+
+(deftest test-basic-vertices-query
+  (let [graph (u/new-tinkergraph)
+        a     (v/create-with-id!  graph 100 {:name "Steven" :age 30})
+        b     (v/create-with-id!  graph 101 {:name "Alonso" :age 32})
+        c     (v/create-with-id!  graph 102 {:name "Thomas" :age 38})
+        _     (e/connect-with-id! 103 a :friend b)
+        _     (e/connect-with-id! 104 a :friend c)
+        vs    (q/query a
+                 (q/--> [:friend])
+                 (q/into-set!))]
+    (is (= 2 (count vs)))
+    (is (= #{b c} vs))))
+
+(deftest test-edge-count
+  (let [graph (u/new-tinkergraph)
+        a     (v/create-with-id!  graph 100 {:name "Steven" :age 30})
+        b     (v/create-with-id!  graph 101 {:name "Alonso" :age 32})
+        c     (v/create-with-id!  graph 102 {:name "Thomas" :age 38})
+        _     (e/connect-with-id! 103 a :friend b)
+        _     (e/connect-with-id! 104 a :friend c)
+        _     (e/connect-with-id! 105 a :remembers c)
+        _     (e/connect-with-id! 106 c :remembers a)
+        n     (q/query a
+                 (q/--> [:friend :remembers])
+                 (q/count!))]
+    (is (= 3 n))))
+
+(deftest test-edge-count-with-default-comparator
+  (let [graph (u/new-tinkergraph)
+        a     (v/create-with-id!  graph 100 {:name "Steven" :age 30})
+        b     (v/create-with-id!  graph 101 {:name "Alonso" :age 32})
+        c     (v/create-with-id!  graph 102 {:name "Thomas" :age 38})
+        _     (e/connect-with-id! 103 a :friend b {:age 28})
+        _     (e/connect-with-id! 104 a :friend c {:age 30})
+        n1    (q/query a
+                 (q/--> [:friend])
+                 (q/has :age 38)
+                 (q/count!))
+        n2    (q/query a
+                 (q/-->  [:friend])
+                 (q/has :age 29)
+                 (q/count!))
+        n3    (q/query a
+                 (q/--> [:hates])
+                 (q/has :age 28)
+                 (q/count!))]
+    (is (= n1 1))
+    (is (= n2 0))
+    (is (= n3 0))))
+
+(deftest test-edge-count-with-gte-comparator
+  (let [graph (u/new-tinkergraph)
+        a     (v/create-with-id!  graph 100 {:name "Steven" :age 30})
+        b     (v/create-with-id!  graph 101 {:name "Alonso" :age 32})
+        c     (v/create-with-id!  graph 102 {:name "Thomas" :age 38})
+        _     (e/connect-with-id! 103 a :friend b {:age 28})
+        _     (e/connect-with-id! 104 a :friend c {:age 30})
+        n1    (q/query a
+                 (q/--> [:friend])
+                 (q/has :age >= 28)
+                 (q/count!))
+        n2    (q/query a
+                 (q/--> [:friend])
+                 (q/has :age >= 33)
+                 (q/count!))
+        n3    (q/query a
+                 (q/--> [:hates])
+                 (q/has :age >= 28)
+                 (q/count!))]
+    (is (= n1 2))
+    (is (= n2 1))
+    (is (= n3 0))))
+
+(deftest test-edge-count-with-lte-comparator
+  (let [graph (u/new-tinkergraph)
+        a     (v/create-with-id!  graph 100 {:name "Steven" :age 30})
+        b     (v/create-with-id!  graph 101 {:name "Alonso" :age 32})
+        c     (v/create-with-id!  graph 102 {:name "Thomas" :age 38})
+        _     (e/connect-with-id! 103 a :friend b {:age 28})
+        _     (e/connect-with-id! 104 a :friend c {:age 30})
+        n1    (q/query a
+                 (q/--> [:friend])
+                 (q/has :age <= 40)
+                 (q/count!))
+        n2    (q/query a
+                 (q/--> [:friend])
+                 (q/has :age <= 37)
+                 (q/count!))
+        n3    (q/query a
+                 (q/--> [:hates])
+                 (q/has :age <= 28)
+                 (q/count!))]
+    (is (= n1 2))
+    (is (= n2 1))
+    (is (= n3 0))))
+
+(deftest test-has-property-key
+  (let [graph (u/new-tinkergraph)
+        a     (v/create-with-id!  graph 100 {:name "Steven" :age 30})
+        b     (v/create-with-id!  graph 101 {:name "Alonso" :age 32})
+        c     (v/create-with-id!  graph 102 {:name "Thomas" :age 38})
+        d     (v/create-with-id!  graph 103 {:name "Claire"})
+        _     (e/connect-with-id! 104 a :friend b {:age 28})
+        _     (e/connect-with-id! 105 a :friend c {:age 30})
+        _     (e/connect-with-id! 106 a :friend d)
+        n1    (q/query a
+                 (q/--> [:friend])
+                 (q/count!))
+        n2    (q/query a
+                 (q/--> [:friend])
+                 (q/has :age)
+                 (q/count!))]
+    (is (= n1 3))
+    (is (= n2 2))))
+
