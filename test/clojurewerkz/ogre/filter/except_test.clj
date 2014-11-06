b91f73eb061c7e559b7088bec297a0bfd052d4e9
Stephen Mallette <spmva@genoprime.com>
2014-11-05 20:16:49 -0500

    Add a test for except step.

--- test/clojurewerkz/ogre/filter/except_test.clj
+++ test/clojurewerkz/ogre/filter/except_test.clj
@@ -12,11 +12,13 @@
                       (q/except [(v/find-by-id g (int 2))])
                       q/into-vec!)]
       (is (= #{"josh" "lop"} (u/get-names-set vs)))))
-
-  (testing "g.v(1).out().aggregate().except([g.v(2)])"
-    ;;TODO reapproach this once aggregate has been figured out
-    )
-
+  (testing "g.v(1).out().except(g.v(2))"
+    (let [g (u/classic-tinkergraph)
+          vs (q/query (v/find-by-id g (int 1))
+                      q/-->
+                      (q/except (v/find-by-id g (int 2)))
+                      q/into-vec!)]
+      (is (= #{"josh" "lop"} (u/get-names-set vs)))))
   (testing "g.v(1).out('created').in('created').except([g.v(1)]).values('name')"
     (let [g (u/classic-tinkergraph)
           names (q/query (v/find-by-id g (int 1))
