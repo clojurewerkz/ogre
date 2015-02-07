26439fec15366bdcb56caf9bd78d9a7a2e9f52b6
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2015-02-06 19:03:54 -0500

    intial work towards using gremlin test suite

---
+++ test/java/org/clojurewerkz/ogre/ThingTest.java
@@ -0,0 +1,30 @@
+package org.clojurewerkz.ogre;
+
+import com.tinkerpop.gremlin.process.Traversal;
+import com.tinkerpop.gremlin.process.graph.step.filter.DedupTest;
+import com.tinkerpop.gremlin.structure.Vertex;
+import com.tinkerpop.gremlin.process.T;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by fitz on 2/6/15.
+ */
+public class ThingTest extends DedupTest {
+
+        @Override
+        public Traversal<Vertex, String> get_g_V_both_dedup_name() {
+            return g.V().both().dedup().values("name");
+        }
+
+        @Override
+        public Traversal<Vertex, String> get_g_V_both_hasXlabel_softwareX_dedup_byXlangX_name() {
+            return g.V().both().has(T.label, "software").dedup().by("lang").values("name");
+        }
+
+        @Override
+        public Traversal<Vertex, String> get_g_V_both_propertiesXnameX_orderXa_bX_dedup_value() {
+            return g.V().both().<String>properties("name").order().by((a, b) -> a.value().compareTo(b.value())).dedup().value();
+        }
+
+}
